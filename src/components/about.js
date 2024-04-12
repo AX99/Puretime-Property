134578745176a3eb893fffa7e5672e8498fae205
AX99 <rapha.mensah@gmail.com>
2024-04-12 14:18:07 +0100

    about us

--- src/components/about.js
+++ src/components/about.js
@@ -8,37 +8,36 @@ import AwardBadge from "../images/award-badge.svg";
 const About = () => {
   const data = useStaticQuery(graphql`
     {
-      aboutimage: file(relativePath: { eq: "about-creative.png" }) {
+      aboutimage: file(relativePath: {eq: "about-creative.png"}) {
         childImageSharp {
-          gatsbyImageData(
-            width: 592
-            placeholder: BLURRED
-            formats: [AUTO, WEBP, AVIF]
-          )
+          gatsbyImageData(width: 592, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+        }
+      }
+      allAboutJson {
+        nodes {
+          section
+          headline
+          paragraph1
+          paragraph2
         }
       }
     }
   `);
+  const page = data.allAboutJson.nodes[0];
   return (
     <div>
       <div className="container mx-auto">
         <div className="grid lg:grid-cols-12 grid-cols-1 lg:gap-8 gap-20 lg:py-32 py-12 items-center">
           <div className="lg:col-span-6 flex flex-col gap-6">
-            <Eyebrow label="ABOUT US" />
+            <Eyebrow label={page.section} />
             <h2 className="font-display md:text-display-xl text-display-md font-normal pb-4">
-              We help to bring your <span className="italic">dream home</span>{" "}
-              to reality
+              <span className="italic underline underline-offset-2 decoration">{page.headline}</span>
             </h2>
             <p className="md:text-body-lg text-body-md font-light text-neutral-700">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
-              fringilla dui amet faucibus nam. Erat id laoreet posuere etiam
-              morbi.
+              {page.paragraph1}
             </p>
             <p className="md:text-body-lg text-body-md font-light text-neutral-700">
-              Tempor dolor elementum tellus non ipsum faucibus. Justo, magna
-              mauris posuere auctor justo. Habitant proin aliquet volutpat leo
-              ultricies. Dui blandit eget vitae turpis ultrices aliquet nunc.
-              Faucibus sit odio bibendum lobortis diam.
+              {page.paragraph2}
             </p>
           </div>
           <div className="lg:col-span-6 flex flex-col gap-8 relative">
@@ -52,6 +51,9 @@ const About = () => {
               className="absolute left-[42%] -top-14"
             />
           </div>
+        </div>
+        <div>
+          <hr className="text-neutral-500"></hr>
         </div>
       </div>
     </div>
