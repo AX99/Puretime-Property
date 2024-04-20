daa68779ee1762770b27ca3f6ab3ed7145ff60f0
AX99 <rapha.mensah@gmail.com>
2024-04-20 04:44:38 +0100

    netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-postcss`,
+    `gatsby-plugin-netlify`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
