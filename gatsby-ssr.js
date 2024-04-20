3c840f2f90d7c66094dda105a802b70d1a039bb3
AX99 <rapha.mensah@gmail.com>
2024-04-20 03:52:46 +0100

    fix ssr + dom issue

---
+++ gatsby-ssr.js
@@ -0,0 +1,7 @@
+
+import React from 'react';
+import { ModalProvider } from './src/context/modalContext';
+
+export const wrapRootElement = ({ element }) => {
+    return <ModalProvider>{element}</ModalProvider>;
+};
