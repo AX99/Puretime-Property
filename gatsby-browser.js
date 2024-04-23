736cdda2a2351202b4b6f080b08b51ef0181879d
AX99 <rapha.mensah@gmail.com>
2024-04-23 12:45:15 +0100

    update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,8 @@
 import "./src/styles/global.css";
+
+import React from 'react';
+import { ModalProvider } from './src/context/modalContext';
+
+export const wrapRootElement = ({ element }) => {
+    return <ModalProvider>{element}</ModalProvider>;
+};
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,7 +5,3 @@ import { ModalProvider } from './src/context/modalContext';
 export const wrapRootElement = ({ element }) => {
     return <ModalProvider>{element}</ModalProvider>;
 };
-
-export const onRenderBody = ({ setHtmlAttributes }) => {
-    setHtmlAttributes({ lang: "en-GB" });
-};
