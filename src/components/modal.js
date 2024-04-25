d029a64f1b6a43ea7640daae4e1dac5c6597857b
AX99 <rapha.mensah@gmail.com>
2024-04-25 13:12:56 +0100

    modal

--- src/components/modal.js
+++ src/components/modal.js
@@ -11,6 +11,7 @@ const Modal = () => {
             closeModal();
         
 
+
     };
 
     return (
@@ -29,51 +30,48 @@ const Modal = () => {
                                 <div className="w-full pt-6 m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 lg:max-w-xl">
                                     <Eyebrow className="underline opacity-100" label="Complete the form to receive your offer."/>
                                     <form id="contact_form" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" className="mt-6">
-                                        <input type="hidden" name="form-name" value="contact"/>
                                         <div className="mb-2">
                                             <label htmlFor="firstName" className="block
-                                                                                                                                                font-size-4 font-weight-semibold text-black-2 line-height-reset
-                                                                                                                                                text-sm ">
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            font-size-4 font-weight-semibold text-black-2 line-height-reset
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            text-sm ">
                                                 First Name:
                                             </label>
-                                            <input type="text" className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="First Name" id="firstName" name="firstName" required/>
+                                            <input type="text" className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="First Name" name="firstName" required/>
                                         </div>
                                         <div className="mb-2">
                                             <label htmlFor="lastName" className="block
-                                                                                                                                                font-size-4 font-weight-semibold text-black-2 line-height-reset
-                                                                                                                                                text-sm ">
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            font-size-4 font-weight-semibold text-black-2 line-height-reset
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            text-sm ">
                                                 Last Name:
                                             </label>
-                                            <input type="text" className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Last Name" id="lastName" name="lastName" required/>
+                                            <input type="text" className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Last Name" name="lastName" required/>
                                         </div>
                                         <div className="mb-2">
                                             <label htmlFor="phonenumber" className="block
-                                                                                                                                                font-size-4 font-weight-semibold text-black-2 line-height-reset
-                                                                                                                                                text-sm ">
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            font-size-4 font-weight-semibold text-black-2 line-height-reset
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            text-sm ">
                                                 Phone Number:
                                             </label>
-                                            <input type="phonenumber" className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Phone Number" id="phonenumber" name="phonenumber" required/>
+                                            <input type="phonenumber" className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Phone Number" name="phonenumber" required/>
                                         </div>
                                         <div className="mb-2">
                                             <label htmlFor="email" className="block
-                                                                                                                                                font-size-4 font-weight-semibold text-black-2 line-height-reset
-                                                                                                                                                text-sm ">
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            font-size-4 font-weight-semibold text-black-2 line-height-reset
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            text-sm ">
                                                 Email:
                                             </label>
-                                            <input type="email" className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40" id="email" name="email" placeholder="Email" required/>
+                                            <input type="email" className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40" name="email" placeholder="Email" required/>
                                         </div>
                                         <div className="mb-2">
                                             <label htmlFor="address" className="block
-                                                                                                                                                font-size-4 font-weight-semibold text-black-2 line-height-reset
-                                                                                                                                                text-sm ">
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            font-size-4 font-weight-semibold text-black-2 line-height-reset
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            text-sm ">
                                                 First Line of Address:
                                             </label>
                                             <input type="text" className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40" id="address" name="address" placeholder="First Line of Address" required/>
                                         </div>
                                         <div className="mb-2">
-                                            <label htmlFor="postcode" className="block
-                                                                                                                                                font-size-4 font-weight-semibold text-black-2 line-height-reset
-                                                                                                                                                text-sm ">
+                                            <label htmlFor="postcode" className="block font-size-4 font-weight-semibold text-black-2 line-height-reset text-sm">
                                                 Postcode:
                                             </label>
                                             <input type="text" className="block w-full px-4 py-2 mt-2 text-indigo-700 bg-white border rounded-md focus:border-indigo-400 focus:ring-indigo-300 focus:outline-none focus:ring focus:ring-opacity-40" id="postcode" name="postcode" placeholder="Postcode"
@@ -83,9 +81,7 @@ const Modal = () => {
                                                 required/>
                                         </div>
                                         <div className="mt-8 justify-center">
-                                            <input type="submit"
-                                                className={`cursor-pointer mx-auto rounded-full flex items-center justify-center bg-primary-600 text-white font-semibold px-5 py-3 text-body-xs uppercase w-100 form-control text-center `}
-                                                value="Submit"/>
+                                            <input type="submit" className="cursor-pointer mx-auto rounded-full flex items-center justify-center bg-primary-600 text-white font-semibold px-5 py-3 text-body-xs uppercase w-100 form-control text-center" value="Submit"/>
                                         </div>
                                     </form>
                                 </div>
