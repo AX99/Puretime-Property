f19fce4596d964e2a585f1203ca8cfa2c77e6205
AX99 <rapha.mensah@gmail.com>
2024-10-25 09:01:46 +0000

    layout fix

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,11 @@
 import React from "react";
 import BlogLayout from "../components/blog/BlogLayout";
 import BlogList from "../components/blog/BlogList";
-import Footer from "../components/footer";
 
 const BlogPage = () => (
   <>
     <BlogLayout>
       <BlogList />
-      <Footer />
     </BlogLayout>
   </>
 );
